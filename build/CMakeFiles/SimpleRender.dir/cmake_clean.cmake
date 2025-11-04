666f75c4c8c9c25fafd5a474cfb3da79559a4bae
deotermia <alkmrgn@outlook.com>
2025-11-04 14:19:38 +0100

    Alpha

---
+++ build/CMakeFiles/SimpleRender.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleRender.dir/libs/glad/src/glad.c.o"
+  "CMakeFiles/SimpleRender.dir/libs/glad/src/glad.c.o.d"
+  "CMakeFiles/SimpleRender.dir/src/Application.cpp.o"
+  "CMakeFiles/SimpleRender.dir/src/Application.cpp.o.d"
+  "CMakeFiles/SimpleRender.dir/src/Renderer.cpp.o"
+  "CMakeFiles/SimpleRender.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/SimpleRender.dir/src/Shader.cpp.o"
+  "CMakeFiles/SimpleRender.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/SimpleRender.dir/src/Window.cpp.o"
+  "CMakeFiles/SimpleRender.dir/src/Window.cpp.o.d"
+  "CMakeFiles/SimpleRender.dir/src/main.cpp.o"
+  "CMakeFiles/SimpleRender.dir/src/main.cpp.o.d"
+  "SimpleRender"
+  "SimpleRender.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/SimpleRender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SimpleRender.dir/src/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/SimpleRender.dir/src/main.cpp.o: \
+ /mnt/c/Users/Nikita/Desktop/Render/src/main.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/c/Users/Nikita/Desktop/Render/src/Application.hpp
