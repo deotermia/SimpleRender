dff691be1ce910f5f11a76944035c9cc51dd8cb6
deotermia <alkmrgn@outlook.com>
2025-11-04 14:22:55 +0100

    Alpha

--- build/CMakeFiles/SimpleRender.dir/src/main.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/SimpleRender.dir/src/main.cpp.o: \
- /mnt/c/Users/Nikita/Desktop/Render/src/main.cpp \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/Nikita/Desktop/Render/src/Application.hpp
